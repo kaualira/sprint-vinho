76c4104ea3ce1fcdd0aa1fadae9a5658e34bc6b5
Kauan <130669072+kavs1346@users.noreply.github.com>
2024-04-26 16:12:29 -0300

    =/ 16:13

---
+++ src/components/Data.js
@@ -0,0 +1,12 @@
+export const countries = [
+    {
+      image: "/images/Carrossel1.png",
+      title: "",
+    },
+    // {
+    //   image: "/images/Carrossel2.png",
+    //   title: "",
+    // },
+
+
+  ];
