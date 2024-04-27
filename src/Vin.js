f6a879ee8b0e1c8b6288c79cb5f44089a03c35d5
Kauã Lira <lirayasumurakaua@gmail.com>
2024-04-26 21:12:46 -0300

    Update Vin.js

---
+++ src/Vin.js
@@ -0,0 +1,13 @@
+import './vinhos.css';
+// import Cabecalho from './components/Cabecalho';
+import NavBarP from './components/NavBarP';
+
+function Vin () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Vin;
