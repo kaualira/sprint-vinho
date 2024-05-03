2c296c38da7d17dbdbac4c96b7abeb423036fb4d
00julia <130668482+00julia@users.noreply.github.com>
2024-05-03 09:00:07 -0300

    juju 9:00

---
+++ src/Assinatura.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Italiana&family=Poppins:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+    <!-- Ajuste o caminho do seu CSS se necessário -->
+    <link rel="stylesheet" href="Assinatura.css">
+    <?php
+    include '../Components/NavBarP.php';
+    ?>
+</head>
+
+<body>
+    <div class="tituloAss">
+        <div class="a01"></div>
+        <div class="a02"><p>ASSINATURA</p></div>
+        <div class="a03"></div>
+    </div>
+
+    <div class="texto1Assinatura">
+        <p align-items="justify">Imagine ter acesso exclusivo a uma seleção premium de vinhos, entregues na sua porta e descontos especiais em nossas bebidas. Isso é possível com uma assinatura em nosso site de vendas! Com entregas programadas e uma experiência de compra personalizada, você terá a oportunidade de explorar novos sabores e descobrir verdadeiras preciosidades da enologia.</p>
+    </div>
+
+    <br><br>
+
+    <div class="texto2Ass">
+        <p>Isso tudo por apenas</p>
+        <p>R$ 359,90 p/mês</p>
+    </div>
+
+    <br><br>
+
+    <div class="buttonAss">
+        <button>Adquira agora!</button>
+    </div>
+
+    <footer>
+      
+    </footer>
+</body>
+</html>
